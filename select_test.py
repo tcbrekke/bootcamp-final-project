31e532f9fb65f39e4d0e681bd786b5b83fe17944
Thomas Brekke <tcbrekke@gmail.com>
2018-08-06 20:34:10 -0700

    fix test app

--- select_test.py
+++ select_test.py
@@ -6,4 +6,7 @@ from sqlalchemy import create_engine
 #Set DATABASE_URL to the database URL served by Heroku Postgres
 DATABASE_URL = os.environ['DATABASE_URL']
 
+engine = create_engine(DATABASE_URL)
+connection = engine.connect()
+
 connection.execute("SELECT * FROM wine_table")
