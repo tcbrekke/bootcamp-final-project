e7af2dde97ab836391991b39dfc3c7b62b7220c1
Aaron Burke <35472931+imtheaaron@users.noreply.github.com>
2018-08-08 13:28:31 -0700

    add event listener for text description click

--- static/js/desc_predictor.js
+++ static/js/desc_predictor.js
@@ -1,12 +1,21 @@
 //set initial variables
-var text = 
 
+var text = document.getElementById("predictiontext").addEventListener("click", function() {
+	text_value = d3.select("#predictiontext").node().value;
+	console.log(text_value);
+}, false);
+
+function getText(value) {
+    var text = value;
+    update_plots(sample_id);
+}
 
 //build the query url from text that will be
 
 function desc_predictor(text) {
+    console.log(text)
     var text_url = '/description_score/' + text
-    d3.json(text_url, function(response) {
+    // d3.json(text_url, function(response) {
         
     
 }
