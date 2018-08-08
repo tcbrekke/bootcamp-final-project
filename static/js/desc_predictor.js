748ebb74bb98efa0387eca8173f0f19b4770f51d
Aaron Burke <35472931+imtheaaron@users.noreply.github.com>
2018-08-08 13:30:00 -0700

    testing function

--- static/js/desc_predictor.js
+++ static/js/desc_predictor.js
@@ -1,20 +1,20 @@
 //set initial variables
 
-var text = document.getElementById("predictiontext").addEventListener("click", function() {
-	text_value = d3.select("#predictiontext").node().value;
-	console.log(text_value);
-}, false);
+// var text = document.getElementById("predictiontext").addEventListener("click", function() {
+// 	text_value = d3.select("#predictiontext").node().value;
+// 	console.log(text_value);
+// }, false);
 
-function getText(value) {
-    var text = value;
-    update_plots(sample_id);
-}
+// function getText(value) {
+//     var text = value;
+//     update_plots(sample_id);
+// }
 
 //build the query url from text that will be
 
 function desc_predictor(text) {
-    console.log(text)
-    var text_url = '/description_score/' + text
+    console.log(text);
+    var text_url = '/description_score/' + text;
     // d3.json(text_url, function(response) {
         
     
