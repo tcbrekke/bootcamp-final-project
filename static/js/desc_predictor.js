f14fc1c914cf5a759ad3fe7fd08a9e5946e889cc
Aaron Burke <35472931+imtheaaron@users.noreply.github.com>
2018-08-08 13:01:15 -0700

    add beginnings of js function to get input text and output rating

---
+++ static/js/desc_predictor.js
@@ -0,0 +1,12 @@
+//set initial variables
+var text = 
+
+
+//build the query url from text that will be
+
+function desc_predictor(text) {
+    var text_url = '/description_score/' + text
+    d3.json(text_url, function(response) {
+        
+    
+}
