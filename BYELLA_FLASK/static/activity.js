15fb3e72b5b88786a3fa8b39a7ca4f7e180d7486
Zulim <zulim2@llnl.gov>
2018-08-08 14:34:31 -0700

    trying to get js to work

--- BYELLA_FLASK/static/activity.js
+++ BYELLA_FLASK/static/activity.js
@@ -1,4 +1,5 @@
-d3.selectAll('submit').on("click", function(){
+d3.select('#submit').on("click", function(){
+  console.log('submit was clicked');
   year_input = document.getElementByName("Year").value;
   variety_input = document.getElementByName("Variety").value;
   price_input = document.getElementByName("Price").value;
@@ -6,3 +7,5 @@ d3.selectAll('submit').on("click", function(){
   country_input = document.getElementByName("Country").value;
   description_len_input = document.getElementByName("Description_Len").value;
 })
+
+console.log('waddup')
