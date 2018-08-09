f99c0c5432ca285ed8fe8789bcbdf294f810ddfc
Zulim <zulim2@llnl.gov>
2018-08-08 17:04:51 -0700

    updated dropdown_reader.js

--- static/js/dropdown_reader.js
+++ static/js/dropdown_reader.js
@@ -1,27 +1,30 @@
-var submitButton = document.getElementById("chooserEntry");
-var variety_selection = document.getElementById("variety_selector");
-var price_selection = document.getElementById("price_selector");
-var region_selection = document.getElementById("region_selector");
-var year_selection = document.getElementById("year_selector");
-var country_selection = document.getElementById("country_selector");
+var submitButton = document.getElementById("#chooserEntry");
+var variety_selector = document.querySelector("#variety_selector");
+console.log(variety_selector);
+var price_selector = document.getElementById("price_selector");
+var region_selector = document.getElementById("region_selector");
+var year_selector = document.getElementById("year_selector");
+var country_selector = document.getElementById("country_selector");
 
 d3.select(submitButton).on("mouseup", function() {
-    variety = variety_selection.value;
-    price = parseFloat(price_selection.value);
-    region = region_selection.value;
-    year = parseFloat(year_selection.value);
-    country = country_selection.value;
+    variety = variety_selector.value;
+    price = parseFloat(price_selector.value);
+    region = region_selector.value;
+    year = parseFloat(year_selector.value);
+    country = country_selector.value;
     var outputDiv = d3.select("#wine-selections");
     outputDiv.selectAll("*").remove();
-    // metadataDiv.append("text").text(`${year}`)
     
-    d3.json(`/choice_score/${variety}/${price}/${region}/${year}/${country}/`, function(jsonData) {
-        jsonData.forEach(function(d) {
-            result = d;
-        });
-        outputDiv.append("text").text(
-        `${result}`
-        );
-        console.log(result)
+    d3.json(`/choice_score/${variety}/${price}/${region}/${year}/${country}/`, function(error, response) {
+        console.log(error);
+        var score = response;
+        console.log(score);
+        if (score) {
+            d3.select("#wine-selections").html(`<h3>Your wine deserves a score of:</h3><h1>${score}</h1>`);
+        }
+        else {
+            d3.select("#wine-selections").html('error');
+        }
     })
+
 });
