d74d8377b00567802d86affffe5dacff8da19614
Zulim <zulim2@llnl.gov>
2018-08-08 17:28:00 -0700

    pushing (hopefully) finished dropdown_reader.js

--- static/js/dropdown_reader.js
+++ static/js/dropdown_reader.js
@@ -1,10 +1,10 @@
-var submitButton = document.getElementById("#chooserEntry");
-var variety_selector = document.querySelector("#variety_selector");
+var submitButton = document.getElementById("chooserEntry");
+var variety_selector = document.getElementById("variety-selector");
 console.log(variety_selector);
-var price_selector = document.getElementById("price_selector");
-var region_selector = document.getElementById("region_selector");
-var year_selector = document.getElementById("year_selector");
-var country_selector = document.getElementById("country_selector");
+var price_selector = document.getElementById("price");
+var region_selector = document.getElementById("region-selector");
+var year_selector = document.getElementById("year-selector");
+var country_selector = document.getElementById("country-selector");
 
 d3.select(submitButton).on("mouseup", function() {
     variety = variety_selector.value;
@@ -15,12 +15,26 @@ d3.select(submitButton).on("mouseup", function() {
     var outputDiv = d3.select("#wine-selections");
     outputDiv.selectAll("*").remove();
     
+    // Sends selections to Thom's route and then appends results to div in index
     d3.json(`/choice_score/${variety}/${price}/${region}/${year}/${country}/`, function(error, response) {
         console.log(error);
         var score = response;
         console.log(score);
         if (score) {
             d3.select("#wine-selections").html(`<h3>Your wine deserves a score of:</h3><h1>${score}</h1>`);
+        }
+        else {
+            d3.select("#wine-selections").html('error');
+        }
+    })    
+
+    // Sends selections to Thom's route and then appends results to div in index
+    d3.json(`/wine_chooser/${variety}/${price}/${region}/${year}/${country}/`, function(error, response) {
+        console.log(error);
+        var results = response;
+        console.log(results);
+        if (results) {
+            d3.select("#wine-selections").html(`${results}`);
         }
         else {
             d3.select("#wine-selections").html('error');
